bfd65f9b81fe2342c7c2fa975c6f919565b0a2c0
fgomes1 <fsgs1997@hotmail.com>
2021-02-24 16:01:37 -0300

    Instancia automatica da base de dados

--- src/main/java/com/fran/bookstore/BookstoreApplication.java
+++ src/main/java/com/fran/bookstore/BookstoreApplication.java
@@ -1,13 +1,37 @@
 package com.fran.bookstore;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BookstoreApplication {
+import com.fran.bookstore.domain.Categoria;
+import com.fran.bookstore.domain.Livro;
+import com.fran.bookstore.repositorys.CategoriaRepository;
+import com.fran.bookstore.repositorys.LivroRepository;
 
+@SpringBootApplication
+public class BookstoreApplication implements CommandLineRunner{
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private LivroRepository livroRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica", "Livros de T.I");
+		
+		Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "lorem ipsum", cat1);
+		
+		cat1.getLivros().addAll(Arrays.asList(l1));
+		
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroRepository.saveAll(Arrays.asList(l1));
 	}
 
 }
